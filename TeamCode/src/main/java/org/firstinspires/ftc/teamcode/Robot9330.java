08bcc6a207240e21ce7c272bebb43bb9e40ef6b9
ColtonPaulBadock <hockey9192008@gmail.com>
2025-10-01 16:44:59 -0700

    Robot drive system completed. The robot can drive, nothing else. This is like a good template to start new robots if needed.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot9330.java
@@ -0,0 +1,159 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import com.qualcomm.hardware.bosch.BNO055IMUNew;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+* Robot hardware control; Class controls all arms, servos, motors, etc.
+*
+*
+**/
+public class Robot9330 {
+
+    //OpMode
+    OpMode opMode; //OpMode from TeleOp.
+
+
+    //Motors
+    DcMotor motorDriveFrontLeft; //Front Left motor for wheel base.
+    DcMotor motorDriveFrontRight; //Front Right motor for wheel base.
+    DcMotor motorDriveBackLeft; //Rear Left motor for wheel base.
+    DcMotor motorDriveBackRight; //Rear Right motor for wheel base.
+
+
+
+    //IMU systems.
+    BNO055IMU imu; //Old IMU (Should not be used.)
+    IMU newIMU; //New IMU
+    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+    Orientation myRobotOrientation; //Holds the robots orientation from the IMU.
+
+
+    //Drive System Variables
+    double botHeading; //Rotation of the robot from IMU.
+    double rotY; //rotation veriable for Y axis
+    double rotX; //rotation veriable for X axis
+    double motorPowerDenominator; //This ratio will make sure the motors keep the same power-balance, I.E. the motors will run with a similar ratio and not spin out while mechanium driving.
+    double frontLeftPower, frontRightPower, backRightPower, backLeftPower; //Power for each of the motors.
+
+
+    //Constructor; Robot inits from here.
+    public Robot9330(OpMode opMode) {
+        this.opMode = opMode;
+        
+        //Old IMU intialization.
+        //imu = opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        //imu.initialize(parameters);
+        
+        //Get IMU from hardware.
+        newIMU = opMode.hardwareMap.get(IMU.class, "imu");
+
+        //Setup robot orientation information for IMU initalization; Default setup, will change if robot is comp bot.
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+
+
+        //Orientation of the IMU, for initalizing the new IMU interface.
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+        
+        //Intialize new IMU interface.
+        //newIMU = opMode.hardwareMap.get(IMU.class, "imu");
+        //newIMU.initialize(parameters);
+        newIMU.initialize(new IMU.Parameters(orientationOnRobot));
+        newIMU.resetYaw(); //Resets the robots IMU to zero, so that the YAW is at 0, so when we initalize from behind the robot, out YAW is zero and the rotational measurement is based on the robots orientation at initialization
+
+        //Init motors; Get the motor class configuraed to the hardware.
+        motorDriveFrontLeft = opMode.hardwareMap.get(DcMotor.class, "leftFront");
+        motorDriveFrontRight = opMode.hardwareMap.get(DcMotor.class, "rightFront");
+        motorDriveBackLeft = opMode.hardwareMap.get(DcMotor.class, "leftBack");
+        motorDriveBackRight = opMode.hardwareMap.get(DcMotor.class, "rightBack");
+        //memeServo = opMode.hardwareMap.get(Servo.class, "memeServo");
+
+
+
+        //Reverse Motors
+        motorDriveFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        //motorDriveFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorDriveBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorDriveBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        
+        //Initalize the robots IMU orientation.
+        myRobotOrientation = newIMU.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+    }
+    
+    //Runs the motors continuesly, as a test.
+    public void runMotorsIndefentlyTest() {
+        motorDriveFrontLeft.setPower(1);
+        motorDriveFrontRight.setPower(1);
+        motorDriveBackLeft.setPower(1);
+        motorDriveBackRight.setPower(1);
+    }
+    
+    
+    //Spins one motor; Used for testing.
+    public void spinOneMotor() {
+        motorDriveBackLeft.setPower(1);
+    }
+    
+    
+    //Robot move method. Moves the robot based on the controller stick inputs.
+    public void move(double x, double y, double rx) {
+        
+        //Get the rotation of the robot as a degree from its starting position.
+        //The rotation we are getting is measured in DEGREES from 360-0.
+        botHeading = newIMU.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        
+        rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+        
+        rotX = rotX * 1.1;  // Counteract imperfect strafing
+        
+        //Make sure all the motors keep the same power-balance/ratio? Stolen from: gmzero.
+        motorPowerDenominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        
+        //Drive the motors based on there rotation, position.
+        frontLeftPower = (rotY + rotX + rx) / motorPowerDenominator;
+        backLeftPower = (rotY - rotX + rx) / motorPowerDenominator;
+        frontRightPower = (rotY - rotX - rx) / motorPowerDenominator;
+        backRightPower = (rotY + rotX - rx) / motorPowerDenominator;
+        
+        //Drive the motors based on our
+        motorDriveFrontLeft.setPower(frontLeftPower);
+        motorDriveBackLeft.setPower(backLeftPower);
+        motorDriveFrontRight.setPower(frontRightPower);
+        motorDriveBackRight.setPower(backRightPower);
+        
+        //OLD MAGICAL MATH SYSTEM.
+        //double botHeading = -imu.getAngularOrientation().firstAngle;
+        // Magical math
+        /*double botHeading = newIMU.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES); //get angular orientation for new IMU interface.
+        double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+        double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double frontRightPower = -(rotY - rotX - rx) / denominator; //MOTOR is flipped.
+        double backLeftPower = -(rotY - rotX + rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+        */ //END OLD MAGICAL MATH SYSTEM
+    }
+
+
+    //Resets the IMU's headless; Resets YAW to 0.
+    public void resetHeadless() {
+        newIMU.resetYaw();
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpOne.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+/*
+* Main Class for FTC 2024 (Into The Deep); Entry point for robot.
+* Class manages inputs from the controller; Controls teleop.
+*
+* By Colton Paul Badock - 2024 (Neobots).
+*
+*
+**/
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="TeleOp", group="Linear OpMode")
+public class TeleOpOne extends LinearOpMode {
+    
+    double gamePadOne_LeftStick_X = 0;
+    double gamePadOne_LeftStick_Y = 0;
+    double gamePadOne_RightStick_X = 0;
+
+
+
+    //SPEED MULTIPLIER
+    double speedMultiplier = 1.0; //Multiplier of the speed for the robots driving.
+    ElapsedTime timeSinceLastSpeedChange = new ElapsedTime(); //Time since the last speed input was entered by the driver, used mainly for debounce.
+
+
+    @Override
+    public void runOpMode() {
+        Robot9330 robot = new Robot9330(this); //Instance of our robot and its hardware.
+        
+        waitForStart(); //Wait until Init on Telemetry is hit before starting the robot.
+        
+        //Infinite application loop.
+        while (opModeIsActive()) {
+            
+            //Get inputs from the gamepad/controller and assign them to the variables
+            gamePadOne_LeftStick_X = gamepad1.left_stick_x;
+            gamePadOne_LeftStick_Y = -gamepad1.left_stick_y;
+            gamePadOne_RightStick_X = gamepad1.right_stick_x;
+            
+            robot.move(gamePadOne_LeftStick_X * speedMultiplier, gamePadOne_LeftStick_Y * speedMultiplier, gamePadOne_RightStick_X * speedMultiplier); //Control robot movement based on controller inputs.
+
+            //Check to see if the driver wants to reset the robots headless (YAW). Same as starting intializing the robot for the drive system.
+            if (gamepad1.x) {
+                robot.resetHeadless();
+            }
+
+            speedToggle(); //Allow the driver to change the speed multiplier with the bumpers.
+
+        }
+    }
+
+
+    //Allows the operator to tune the speed on the fly.
+    public void speedToggle() {
+
+        //Based on inputs from driver on the bumpers, toggle the speed of the robot.
+        if (gamepad1.left_bumper == true && timeSinceLastSpeedChange.milliseconds() > 200) {
+            if (speedMultiplier > 0.2) {
+                speedMultiplier -= 0.2; //Change the speed multiplier.
+                timeSinceLastSpeedChange = new ElapsedTime(); //Reset the timer since the last speed change.
+            }
+        } else if (gamepad1.right_bumper == true && timeSinceLastSpeedChange.milliseconds() > 200) {
+            if (speedMultiplier < 1.0) {
+                speedMultiplier += 0.2; //Change the speed multiplier.
+                timeSinceLastSpeedChange = new ElapsedTime(); //Reset the timer since the last speed change.
+            }
+        }
+    }
+}
