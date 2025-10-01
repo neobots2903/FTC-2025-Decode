8ae046d39ba35346f0f719f3b469ea52ae2b53f8
ColtonPaulBadock <hockey9192008@gmail.com>
2025-10-01 16:03:40 -0700

    Created a empty drive system. Backing up this state before deleting some stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/README.txt
@@ -0,0 +1,2 @@
+Tuning docs:
+	https://rr.brott.dev/docs/v1-0/tuning/
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UniversalData.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+/*
+*
+* This class holds data we want to be Universal. The data here will be used by classes and is defined by the programmers before compiling, and
+* likely will not change mid-program.
+* */
+public class UniversalData {
+
+    public boolean isCompetitionBot = true;
+    public boolean isProgrammingTestBot = false;
+}
