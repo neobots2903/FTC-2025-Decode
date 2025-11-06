c427a4ea2fefdff4ee1e9f51f6c88d8f9e2b7ba3
ColtonPaulBadock <hockey9192008@gmail.com>
2025-11-05 16:12:31 -0800

    Added axial, heading and lateral gains

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -82,9 +82,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 1;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 0.95;
+        public double lateralGain = 0.95;
+        public double headingGain = 0.9; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
