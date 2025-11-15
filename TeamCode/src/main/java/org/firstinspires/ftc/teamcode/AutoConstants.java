97eea9becdb1243772899de75e3d898083802f00
ColtonPaulBadock <hockey9192008@gmail.com>
2025-11-15 13:32:32 -0800

    Added more degrees to the rotation for field error in auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoConstants.java
@@ -22,7 +22,7 @@ public class AutoConstants {
         //be equal to the negative of these.
         final int blue_towardsOblesk_shootingPosition_x = 5; //95
         final int blue_towardsOblesk_shootingPosition_y = 5; //5
-        final double blue_towardsOblesk_firingPositionRotation = 27.0; //52.0
+        final double blue_towardsOblesk_firingPositionRotation = 30.0; //52.0
         final int blue_towardsOblesk_parkPosition_x = 30;
         final int blue_towardsOblesk_parkPosition_y = 25;
 
@@ -34,7 +34,7 @@ public class AutoConstants {
         //be equal to the negative of the blue values.
         final int red_towardsOblesk_shootingPosition_x = blue_towardsOblesk_shootingPosition_x;
         final int red_towardsOblesk_shootingPosition_y = -blue_towardsOblesk_shootingPosition_y;
-        final double red_towardsOblesk_firingPositionRotation = -blue_towardsOblesk_firingPositionRotation;
+        final double red_towardsOblesk_firingPositionRotation = -(blue_towardsOblesk_firingPositionRotation + 4.0); //Adds 4 due to the shooter not being centered
         final int red_towardsOblesk_parkPosition_x = blue_towardsOblesk_parkPosition_x;
         final int red_towardsOblesk_parkPosition_y = -blue_towardsOblesk_parkPosition_y;
 }
