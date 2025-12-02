0d4b0d2b15c5517beb860c6e8f8d2a354d35505e
Franklin33333 <franklinvincenzi@gmail.com>
2025-12-01 17:35:25 -0800

    Created the LED System for the new robot, so that it can display certain colors based on which balls are in the shooter

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LEDSystem.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+public class LEDSystem {
+
+
+    RevBlinkinLedDriver blinkinLedDriver;
+    RevBlinkinLedDriver.BlinkinPattern pattern;
+
+    private OpMode opMode;
+
+
+
+    //Constructor
+    public LEDSystem(OpMode opMode) {
+        this.opMode = opMode;
+
+        initLEDs();
+    }
+
+
+    //Intializes all LEDs with the hardware
+    //map so we can use them, runs setups if needed
+    private void initLEDs() {
+
+        //Setup the LED for indexer indication
+        blinkinLedDriver = opMode.hardwareMap.get(RevBlinkinLedDriver.class, "blinkin");
+    }
+
+    public void setIndicatorColor(String color) {
+
+        if (color == "PURPLE") {
+            //Logic to make light purple
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.VIOLET);
+        } else if (color == "GREEN") {
+            //Logic to make light green
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+        } else {
+            //Logic to make light off
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+        }
+
+    }
+
+}
